035e09cafac1e94ddd7c1bb967514c7da5cd965a
周明順 <a36576942@llt.com.tw>
2026-02-06 14:58:10 +0800

    init vue3 hello world

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="center">
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+html, body, #app {
+  height: 100%;
+  margin: 0;
+}
+
+.center {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
